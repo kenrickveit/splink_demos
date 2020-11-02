c89f91af9aa9351d07f4271e56160b0e16389aba
Kenrick Veit <kenrick.veit@gmail.com>
2020-11-02 17:46:42 -0600

    Formatting

--- splink_install.sh
+++ splink_install.sh
@@ -6,8 +6,12 @@ sudo /usr/bin/pip3 install graphframes
 sudo /usr/bin/pip3 install pandas
 sudo /usr/bin/pip3 install IPython
 sudo /usr/bin/pip3 install altair
+
+# Install splink_demos as a Python package
 sudo /usr/bin/aws s3 cp s3://splink-emr-init-test/splink_demos.zip /tmp
 /usr/bin/unzip /tmp/splink_demos.zip -d /tmp 
 sudo /usr/bin/pip3 install /tmp/splink_demos
+
+# graphframes jar package pre-reqs 
 /usr/bin/aws s3 cp --recursive s3://splink-emr-init-test/jars /home/hadoop/extrajars/
 
